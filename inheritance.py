159f5dce79ee0d4760ae585d74746b96bf4bd9fd
Hemil Modi <hemil.modi2018@gmail.com>
2020-01-08 19:57:18 +0530

    inheritence basic

--- inheritance.py
+++ inheritance.py
@@ -1,18 +1,17 @@
 class Student():
+
     def __init__(self):
         print("my id ")
+
     def name(self):
         print("my name")
+
     def city(self):
         print("my city")
 
-#my_student = Student()
-#my_student.city()
-
 class School(Student):
     def __init__(self):
-        School.__init__(self)
+        Student.__init__(self)
         print("my school")
-
-my_newname=School()
-School.city()
+my_school=School()
+my_school.city()
